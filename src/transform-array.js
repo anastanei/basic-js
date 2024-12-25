8ce1c2b1116e8d10d8d7977f2ff1eec38fe9e7f9
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-25 01:57:33 +0100

    feat: implement transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const keys = new Map([
+  [
+    '--discard-next',
+    (array, index, acc) => {
+      array[index + 1] = undefined;
+      return acc;
+    }
+  ],
+  [
+    '--discard-prev',
+    (array, index, acc) => {
+      if (array[index - 1] !== undefined) {
+        acc[acc.length - 1] && acc.pop();
+      }
+      return acc;
+    }
+  ],
+  [
+    '--double-next',
+    (array, index, acc) => {
+      if (array[index + 1] !== undefined) {
+        acc.push(array[index + 1]);
+      }
+      return acc;
+    }
+  ],
+  [
+    '--double-prev',
+    (array, index, acc) => {
+      if (array[index - 1] !== undefined) {
+        acc.push(array[index - 1]);
+      }
+      return acc;
+    }
+  ]
+]);
+
+function transform(srcArr) {
+  if (!Array.isArray(srcArr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+
+  const arr = [...srcArr];
+  const res = arr.reduce((acc, item, index, array) => {
+    if (keys.has(item)) {
+      const func = keys.get(item);
+      return func(array, index, acc);
+    };
+    if (item !== undefined) {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
+
+  return res;
 }
 
 module.exports = {
   transform
 };
+``
