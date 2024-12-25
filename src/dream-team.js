140426b665650a3b77135b5a6457824e2743b499
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-25 02:14:53 +0100

    feat: implement dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  return members
+    .reduce((acc, member) => {
+      if (typeof member === 'string') {
+        acc.push(member.trim()[0]);
+      }
+      return acc;
+    }, [])
+    .sort((a, b) => a.localeCompare(b))
+    .join('')
+    .toUpperCase();
 }
 
 module.exports = {
