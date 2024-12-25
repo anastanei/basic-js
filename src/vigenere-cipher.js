e108e67faf7cf6fd72ea070f4fabb3cfb4fe3848
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-25 17:09:11 +0100

    feat: implement vigenere-chipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,17 +19,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  #checkArgs(string, key) {
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+
+  #getIndex(char) {
+    return char.toUpperCase().charCodeAt(0) - 65
+  }
+
+  #crypt(string, key, isEncrypting = 1) {
+
+    this.#checkArgs(string, key);
+    let keyIndex = 0;
+
+    const result = string
+      .split('')
+      .map((char) => {
+        if (!/[A-Z]/i.test(char)) return char;
+        const keyValue = this.#getIndex(key[keyIndex % key.length]) * isEncrypting;
+        const resChar = String.fromCharCode(((this.#getIndex(char) + keyValue + 26) % 26) + 65);
+        keyIndex += 1;
+        return resChar;
+      })
+      .join('');
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  encrypt(string, key) {
+    return this.#crypt(string, key);
+  }
+
+  decrypt(string, key) {
+    return this.#crypt(string, key, -1);
   }
 }
 
-module.exports = {
-  VigenereCipheringMachine
-};
+  (module.exports = {
+    VigenereCipheringMachine
+  });
