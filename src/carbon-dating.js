206749930cc2267b8fd15b01b426041a474b67cd
Anastasiia Nikonova <anastaney@gmail.com>
2024-12-25 02:45:51 +0100

    feat: implement carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
-
+const LOG_TWO_APPROXIMATION = 0.693;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    !sampleActivity
+    || typeof sampleActivity !== 'string'
+    || sampleActivity <= 0
+    || sampleActivity > MODERN_ACTIVITY
+    || isNaN(sampleActivity)
+  ) {
+    return false;
+  }
+
+  const k = LOG_TWO_APPROXIMATION / HALF_LIFE_PERIOD;
+  const t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  return Math.ceil(t);
+
 }
 
 module.exports = {
